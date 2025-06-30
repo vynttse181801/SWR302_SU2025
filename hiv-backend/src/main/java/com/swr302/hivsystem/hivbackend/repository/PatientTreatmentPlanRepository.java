4aa43917dd4dc94debefa33a8671a659eea268e1
khoa2709 <khoandse181807@fpt.edu.vn>
2025-06-30 23:06:30 +0700

    Finish merge: resolve conflicts and update DoctorProfile, PatientTreatmentPlanController, and PatientTreatmentPlanRepository

--- hiv-backend/src/main/java/com/swr302/hivsystem/hivbackend/repository/PatientTreatmentPlanRepository.java
+++ hiv-backend/src/main/java/com/swr302/hivsystem/hivbackend/repository/PatientTreatmentPlanRepository.java
@@ -4,6 +4,11 @@ import com.swr302.hivsystem.hivbackend.model.PatientTreatmentPlan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PatientTreatmentPlanRepository extends JpaRepository<PatientTreatmentPlan, Long> {
+    List<PatientTreatmentPlan> findByDoctorId(Long doctorId);
+    List<PatientTreatmentPlan> findByPatientId(Long patientId);
+    List<PatientTreatmentPlan> findByDoctorIdAndPatientId(Long doctorId, Long patientId);
 } 
